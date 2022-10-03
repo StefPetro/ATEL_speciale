c0bb2c83b0d307a7726b14903ea1b5814495e5ee
StefPetro <43845091+StefPetro@users.noreply.github.com>
2022-10-03 17:06:26 +0200

    Delete roberta_model.py

--- roberta_model.py
+++
@@ -1,5 +0,0 @@
-from transformers import BertForSequenceClassification
-
-model = BertForSequenceClassification.from_pretrained("bert-base-uncased", 
-                                                      num_labels=10, 
-                                                      problem_type="multi_label_classification")
