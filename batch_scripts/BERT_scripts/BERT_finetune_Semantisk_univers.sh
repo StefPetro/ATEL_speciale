6f2c43158501c20840aaf935a32892dad68df246
StefPetro <43845091+StefPetro@users.noreply.github.com>
2022-10-13 14:03:48 +0200

    Added seperate batch scripts for each target col

--- batch_scripts/BERT_scripts/BERT_finetune_Semantisk_univers.sh
+++ batch_scripts/BERT_scripts/BERT_finetune_Semantisk_univers.sh
@@ -1,27 +1,16 @@
 #!/bin/sh 
-
-
-# Load dependencies
-module load python3/3.9.14
-
-python3 -m pip install --user -r requirements.txt
-
-# module load matplotlib/3.4.2-numpy-1.21.1-python-3.9.6
-module load pandas/1.3.1-python-3.9.14
-module load cuda/11.5
-module load cudnn/v8.3.0.98-prod-cuda-11.5
 ### General options 
 ### -- specify queue -- 
 #BSUB -q gpuv100
 #BSUB -gpu "num=1:mode=exclusive_process"
 ### -- set the job Name -- 
-#BSUB -J lstm_tester
+#BSUB -J run_bert
 ### -- ask for number of cores (default: 1) -- 
-#BSUB -n 20
+#BSUB -n 12
 ### -- specify that the cores must be on the same host -- 
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 2GB of memory per core/slot -- 
-#BSUB -R "rusage[mem=2GB]"
+#BSUB -R "rusage[mem=8GB]"
 ### -- set walltime limit: hh:mm -- 
 #BSUB -W 24:00 
 ### -- set the email address -- 
@@ -29,7 +18,7 @@ module load cudnn/v8.3.0.98-prod-cuda-11.5
 # if you want to receive e-mail notifications on a non-default address
 ##BSUB -u your_email_address
 ### -- send notification at start -- 
-#BSUB -B 
+##BSUB -B 
 ### -- send notification at completion -- 
 #BSUB -N 
 ### -- Specify the output and error file. %J is the job-id -- 
@@ -38,4 +27,14 @@ module load cudnn/v8.3.0.98-prod-cuda-11.5
 #BSUB -e lstm_tester.err
 
 # here follow the commands you want to execute 
-python3 run_lstm.py
+
+# Load dependencies
+module load python3/3.9.14
+#module load gcc/10.3.0
+#module load cuda/11.5
+module load cudnn/v8.3.0.98-prod-cuda-11.5
+
+source ./venv/bin/activate
+# echo $VIRTUAL_ENV
+
+python3 run_BERT_finetune.py --target_col "Semantisk univers"
