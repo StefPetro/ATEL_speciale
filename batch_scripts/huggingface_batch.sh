015c689791be8592c02ebdbd4e312c393030696f
StefPetro <43845091+StefPetro@users.noreply.github.com>
2022-10-13 15:51:00 +0200

    Update huggingface_batch.sh

--- batch_scripts/huggingface_batch.sh
+++ batch_scripts/huggingface_batch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh 
-bsub < ./BERT_scripts/BERT_finetune_Genre.sh
-bsub < ./BERT_scripts/BERT_finetune_Tekstbaand.sh
-bsub < ./BERT_scripts/BERT_finetune_Fremstillingsform.sh
-bsub < ./BERT_scripts/BERT_finetune_Semantisk_univers.sh
-bsub < ./BERT_scripts/BERT_finetune_Stemmer.sh
-bsub < ./BERT_scripts/BERT_finetune_Perspektiv.sh
-bsub < ./BERT_scripts/BERT_finetune_Holistisk_vurdering.sh
+bsub < ./batch_scripts/BERT_scripts/BERT_finetune_Genre.sh
+bsub < ./batch_scripts/BERT_scripts/BERT_finetune_Tekstbaand.sh
+bsub < ./batch_scripts/BERT_scripts/BERT_finetune_Fremstillingsform.sh
+bsub < ./batch_scripts/BERT_scripts/BERT_finetune_Semantisk_univers.sh
+bsub < ./batch_scripts/BERT_scripts/BERT_finetune_Stemmer.sh
+bsub < ./batch_scripts/BERT_scripts/BERT_finetune_Perspektiv.sh
+bsub < ./batch_scripts/BERT_scripts/BERT_finetune_Holistisk_vurdering.sh
