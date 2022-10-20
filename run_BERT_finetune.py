561d25cb8cc308946840265be430b8e72c77f83d
StefPetro <43845091+StefPetro@users.noreply.github.com>
2022-10-20 10:36:50 +0200

    General update
    Added:
    - Weight decay
    - Learning rate
    
    Changed:
    - Log_dir
    - num epochs
    - compute metric func

--- run_BERT_finetune.py
+++ run_BERT_finetune.py
@@ -1,8 +1,11 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from transformers import TrainingArguments, Trainer, TrainerCallback
+from transformers import AdamW, get_linear_schedule_with_warmup
 from datasets import Dataset
 from torchmetrics import Accuracy
 from torchmetrics.classification import MultilabelAccuracy, MulticlassAccuracy
+from torchmetrics.functional import accuracy
+from torchmetrics.functional.classification import multilabel_accuracy
 from sklearn.model_selection import KFold
 from data_clean import *
 from atel.data import BookCollection
@@ -23,7 +26,10 @@ TARGET = args.target_col
 SEED = 42
 NUM_SPLITS = 10
 BATCH_SIZE = 16
-NUM_EPOCHS = 50
+BATCH_ACCUMALATION = 4
+NUM_EPOCHS = 100
+LEARNING_RATE = 2e-5
+WEIGHT_DECAY  = 0.01
 set_seed(SEED)
 
 with open('target_problem_type.yaml', 'r', encoding='utf-8') as f:
@@ -56,22 +62,35 @@ if problem_type == 'multilabel':
     multilabel = True
     p_t = "multi_label_classification"
     logit_func = torch.nn.Sigmoid()
-    acc_metric = Accuracy(subset_accuracy=True)
     
 else:
     multilabel = False
     p_t = "single_label_classification"
     logit_func = torch.nn.Softmax()
-    acc_metric = MulticlassAccuracy(num_classes=NUM_LABELS)
 
 
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
-    metrics = {
-        "accuracy": acc_metric(logit_func(torch.tensor(logits)), torch.tensor(labels).int()),
-    }
+    preds = logit_func(torch.tensor(logits))
+    labels = torch.tensor(labels).int()
+    
+    if problem_type == 'multilabel':
+        preds = logit_func(preds)
+        acc_micro = accuracy(preds, labels, subset_accuracy=True)
+        acc_macro = multilabel_accuracy(preds, labels, num_labels=NUM_LABELS)
+        
+        metrics = {
+            f'accuracy_micro': acc_micro,
+            f'accuracy_macro': acc_macro
+        }
+    else:
+        preds = logit_func(preds)
+        acc = accuracy(preds, labels)
+        
+        metrics = {f'accuracy': acc}
+        
     return metrics
-
+    
 
 dataset = Dataset.from_pandas(df)
 token_dataset = dataset.map(tokenize_function, batched=True)
@@ -88,24 +107,41 @@ for k in range(NUM_SPLITS):
     val_dataset   = token_dataset.select(val_idx)
 
     model = AutoModelForSequenceClassification.from_pretrained("Maltehb/danish-bert-botxo", 
-                                                            num_labels=NUM_LABELS, 
-                                                            problem_type=p_t,
-                                                            label2id=label2id,
-                                                            id2label=id2label)
+                                                               num_labels=NUM_LABELS, 
+                                                               problem_type=p_t,
+                                                               label2id=label2id,
+                                                               id2label=id2label)
+    
+    # optimizer = AdamW(model.parameters(), lr=LEARNING_RATE)
+    # lr_scheduler = get_linear_schedule_with_warmup(
+    #     optimizer = optimizer,
+    #     num_warmup_steps=0,
+    #     num_training_steps=100
+    # )
     
     training_args = TrainingArguments(
         output_dir="test_trainer",
-        seed=SEED,
         save_strategy='no',
+        logging_strategy='steps',
+        logging_steps=1,
+        logging_dir=f'huggingface_logs'\
+                    +f'/{TARGET}'\
+                    +f'/BERT-batch_size_{BATCH_SIZE}'\
+                    +f'-batch_accum_{BATCH_ACCUMALATION}'\
+                    +f'-epochs_{NUM_EPOCHS}'\
+                    +f'-seed_{SEED}'\
+                    +f'-Weight_decay_{WEIGHT_DECAY}'\
+                    +f'-Learning_rate_{LEARNING_RATE}'\
+                    +f'/{TARGET}-CV_{k+1}',
+        report_to='tensorboard',
+        evaluation_strategy='epoch',
+        seed=SEED,
         per_device_train_batch_size=BATCH_SIZE,
         per_device_eval_batch_size=BATCH_SIZE,
         gradient_accumulation_steps=4,
-        evaluation_strategy='epoch',
-        report_to='tensorboard',
-        logging_strategy='steps',
-        logging_steps=1,
-        logging_dir=f'huggingface_logs/{TARGET}/BERT-{TARGET}-CV_{k+1}-batch_size_{BATCH_SIZE}-epochs_{NUM_EPOCHS}-seed_{SEED}',
-        num_train_epochs=NUM_EPOCHS
+        num_train_epochs=NUM_EPOCHS,
+        learning_rate=LEARNING_RATE,
+        weight_decay=WEIGHT_DECAY
     )
 
     trainer = Trainer(
