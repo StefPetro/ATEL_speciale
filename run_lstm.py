b8d18f50ca02467104b75a0900463595c59bd5b8
StefPetro <43845091+StefPetro@users.noreply.github.com>
2022-10-20 12:27:33 +0200

    Changed file to include args and for one cv fold

--- run_lstm.py
+++ run_lstm.py
@@ -7,13 +7,40 @@ import pytorch_lightning as pl
 from pytorch_lightning import Trainer
 import fasttext
 import fasttext.util
-
+import argparse
+import yaml
+from yaml import CLoader
 import warnings
 warnings.filterwarnings("ignore", ".*does not have many workers.*")
 
+parser = argparse.ArgumentParser(description='Arguments for training the LSTM model')
+parser.add_argument(
+    '--target_col',
+    help='The target column to train the BERT model on.', 
+    default=None
+)
+parser.add_argument(
+    '--cv',
+    help='Which cross-validation fold to use. Can be 1-10',
+    default=1,
+    type=int
+)
+args = parser.parse_args()
+
+TARGET = args.target_col
+CV = args.cv - 1  # minus 1 as we want the --cv argument to be 1-10
+
 SEED = 42
+NUM_FOLDS = 10
 NUM_EPOCHS = 5000
+EMBEDDING_SIZE = 300
 set_seed(SEED)
+
+with open('target_info.yaml', 'r', encoding='utf-8') as f:
+    target_info = yaml.load(f, Loader=CLoader)
+
+problem_type = target_info[TARGET]['problem_type']
+NUM_LABELS   = target_info[TARGET]['num_labels']
 
 ## Load the data
 book_col = BookCollection(data_file="./data/book_col_271120.pkl")
@@ -22,51 +49,51 @@ book_col = BookCollection(data_file="./data/book_col_271120.pkl")
 # https://fasttext.cc/docs/en/crawl-vectors.html
 print('Loading fastText model...')
 ft = fasttext.load_model('fasttext_model/cc.da.300.bin')  # Download from fastTexts website
-fasttext.util.reduce_model(ft, 100)
+if EMBEDDING_SIZE < 300:
+    fasttext.util.reduce_model(ft, EMBEDDING_SIZE)
 print('Loading complete!')
 
 settings = {
-    'multi_label': True,
-    'n_features': 100, 
-    "hidden_size": 512,
-    "num_layers": 4,
-    'l1_size': 512,
-    'l2_size': 256,
-    "dropout": 0.2,
-    "batch_size": 64,
-    "learning_rate" : 1e-5,
-    "output_size": 5
+    'multi_label':   True if problem_type == 'multilabel' else False,
+    'n_features':    EMBEDDING_SIZE,
+    "hidden_size":   512,
+    "num_layers":    4,
+    'l1_size':       512,
+    'l2_size':       256,
+    "dropout":       0.2,
+    "batch_size":    32,
+    "learning_rate": 1e-5,
+    "output_size":   NUM_LABELS
 }
 
-num_folds = 10
-results1 = []
-results2 = []
-target_col = 'Semantisk univers'
+print(f'RUNNING CV K = {CV+1}/{NUM_FOLDS}')
 
-for k in range(num_folds):
-    print(f'STARTING CV K = {k+1}/{num_folds}')
-    
-    data = lstm_data(
-        book_col=book_col, 
-        target_col=target_col, 
-        ft=ft, 
-        batch_size=settings['batch_size'], 
-        seq_len=171,
-        seed=SEED,
-        k=k
-    )
-    model = lstm_text(**settings)
-    logger_name = f'{target_col.replace(" ", "_")}-cv{k}-max_epoch_{NUM_EPOCHS}-num_lstm_layers_{settings["num_layers"]}'
-    logger = pl.loggers.TensorBoardLogger(save_dir='lightning_logs', name=logger_name)
-    
-    trainer = Trainer(
-        max_epochs = NUM_EPOCHS,
-        gpus = 1 if torch.cuda.is_available() else 0,
-        log_every_n_steps = 1,
-        enable_checkpointing = False,
-        logger = logger
-    )
-    trainer.fit(model, data)
-    
-    print('Done!')
-    break
+data = lstm_data(
+    book_col=book_col, 
+    target_col=TARGET, 
+    ft=ft, 
+    batch_size=settings['batch_size'], 
+    seq_len=171,
+    seed=SEED,
+    k=CV
+)
+model = lstm_text(**settings)
+logger_name = f'{TARGET.replace(" ", "_")}'\
+                +f'num_epoch_{NUM_EPOCHS}'\
+                +f'-embedding_size_{EMBEDDING_SIZE}'\
+                +f'-lstm_layers_{settings["num_layers"]}'\
+                +f'-lstm_size_{settings["hidden_size"]}'\
+                +f'-l1_size_{settings["l1_size"]}'\
+                +f'-l2_size_{settings["l2_size"]}'\
+                +f'CV_{CV+1}'
+logger = pl.loggers.TensorBoardLogger(save_dir='lightning_logs', name=logger_name)
+
+trainer = Trainer(
+    max_epochs = NUM_EPOCHS,
+    gpus = 1 if torch.cuda.is_available() else 0,
+    log_every_n_steps = 1,
+    enable_checkpointing = False,
+    logger = logger
+)
+trainer.fit(model, data)
+print('Done!')
