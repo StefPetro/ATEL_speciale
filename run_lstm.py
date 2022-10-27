783176eba97a954d777a0c9db1982233a6ec9387
Enny <ennytao@gmail.com>
2022-10-27 10:53:57 +0200

    updates to run_lstm

--- run_lstm.py
+++ run_lstm.py
@@ -1,20 +1,32 @@
+import warnings
+
+import fasttext
+import fasttext.util
 import numpy as np
+import pytorch_lightning as pl
+import torch
+import yaml
+from pytorch_lightning import Trainer
+from yaml import CLoader
+
 from atel.data import BookCollection
 from data_clean import set_seed
 from lstm_model import lstm_data, lstm_text
-import torch
-import pytorch_lightning as pl
-from pytorch_lightning import Trainer
-import fasttext
-import fasttext.util
-
-import warnings
 
 warnings.filterwarnings("ignore", ".*does not have many workers.*")
 
+TARGET = "Stemmer"
 SEED = 42
+NUM_FOLDS = 10
 NUM_EPOCHS = 10000
+EMBEDDING_SIZE = 100
 set_seed(SEED)
+
+with open("target_info.yaml", "r", encoding="utf-8") as f:
+    target_info = yaml.load(f, Loader=CLoader)
+
+problem_type = target_info[TARGET]["problem_type"]
+NUM_LABELS = target_info[TARGET]["num_labels"]
 
 ## Load the data
 book_col = BookCollection(data_file="./data/book_col_271120.pkl")
@@ -29,35 +41,33 @@ fasttext.util.reduce_model(ft, 100)
 print("Loading complete!")
 
 settings = {
-    "multi_label": True,
-    "n_features": 100,
+    "multi_label": True if problem_type == "multilabel" else False,
+    "n_features": EMBEDDING_SIZE,
     "hidden_size": 256,
     "num_layers": 4,
     "dropout": 0.2,
     "batch_size": 128,
     "learning_rate": 1e-5,
-    "output_size": 5,
+    "output_size": NUM_LABELS,
 }
 
-num_folds = 10
 results1 = []
 results2 = []
-target_col = "Semantisk univers"
 
-for k in range(num_folds):
-    print(f"STARTING CV K = {k+1}/{num_folds}")
+for k in range(NUM_FOLDS):
+    print(f"STARTING CV K = {k+1}/{NUM_FOLDS}")
 
     model = lstm_text(**settings)
     data = lstm_data(
         book_col=book_col,
-        target_col=target_col,
+        target_col=TARGET,
         ft=ft,
         batch_size=settings["batch_size"],
         seq_len=128,
         seed=SEED,
         k=k,
     )
-    logger_name = f'{target_col.replace(" ", "_")}-cv{k}-max_epoch_{NUM_EPOCHS}'
+    logger_name = f'{TARGET.replace(" ", "_")}-cv{k}-max_epoch_{NUM_EPOCHS}'
     logger = pl.loggers.TensorBoardLogger(save_dir="lightning_logs/", name=logger_name)
 
     trainer = Trainer(
