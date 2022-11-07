a6ff7de4ec798c5500ceb4dda50a2f19e848d130
Enny <ennytao@gmail.com>
2022-11-07 11:52:05 +0100

    changed error

--- run_roberta_finetune.py
+++ run_roberta_finetune.py
@@ -185,17 +185,16 @@ for k in range(NUM_SPLITS):
 
     trainer.train()
 
-    trainer.model.to('cpu')
+    trainer.model.to("cpu")
     outputs = trainer.model(
         input_ids=torch.tensor(val_dataset["input_ids"]),
         labels=torch.tensor(val_dataset["labels"]),
     )
 
-    torch.save(outputs.logits, f'{logging_name}/{TARGET}_CV{k+1}_best_model_logits.pt')
-    
+    torch.save(outputs.logits, f"{logging_name}/{TARGET}_CV{k+1}_best_model_logits.pt")
+
     ## Removes the saved checkpoints, as they take too much space
     for f in os.listdir(f"huggingface_saves/{TARGET}"):
         shutil.rmtree(os.path.join(f"huggingface_saves/{TARGET}", f))
-        
-	break
-        
+
+    break
