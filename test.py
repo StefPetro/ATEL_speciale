1911a89401488e760badf2470f2c21a8dfd624d2
StefPetro <43845091+StefPetro@users.noreply.github.com>
2022-11-09 13:49:00 +0100

    random changes

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import torch
+import os
 
-filepath = 'huggingface_logs/Fremstillingsform/BERT-BS16-BA4-ep100-seed42-WD0.01-LR2e-05/CV_1/Fremstillingsform_CV1_best_model_logits.pt'
+logging_name = f'huggingface_logs'\
+                +f'/test'
 
-test = torch.load(filepath)
-
-print(test)
+print(logging_name[17:])
