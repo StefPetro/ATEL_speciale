138c01937199d9d485bfa6eb66f613f2b68f33c1
Enny <ennytao@gmail.com>
2022-11-09 13:41:00 +0100

    clean up files

--- random_guess.py
+++ random_guess.py
@@ -1,23 +1,11 @@
 import numpy as np
 import pandas as pd
-from data_clean import *
-from atel.data import BookCollection
 import yaml
 from yaml import CLoader
-from torchmetrics.functional.classification import (
-    multiclass_accuracy,
-    multiclass_auroc,
-    multiclass_f1_score,
-    multiclass_precision,
-    multiclass_recall,
-    multilabel_accuracy,
-    multilabel_auroc,
-    multilabel_exact_match,
-    multilabel_f1_score,
-    multilabel_precision,
-    multilabel_recall,
-)
 
+from atel.data import BookCollection
+from compute_metrics import compute_metrics
+from data_clean import *
 
 SEED = 42
 set_seed(SEED)
@@ -27,70 +15,6 @@ book_ids, texts = clean_book_collection_texts(book_col, lowercase=False)
 
 with open("target_info.yaml", "r", encoding="utf-8") as f:
     target_problems = yaml.load(f, Loader=CLoader)
-
-
-def compute_metrics(preds, labels, problem_type, num_labels):
-
-    preds = torch.Tensor(preds)
-    labels = torch.tensor(labels)
-
-    if problem_type == "multilabel":
-        acc_exact = multilabel_exact_match(preds, labels, num_labels=num_labels)
-        acc_macro = multilabel_accuracy(
-            preds, labels, num_labels=num_labels, average="macro"
-        )
-
-        # How are they calculated?:
-        # The metrics are calculated for each label.
-        # So if there is 4 labels, then 4 recalls are calculated.
-        # These 4 values are then averaged, which is the end score that is logged.
-        # The default average applied is 'macro'
-        # precision_macro = multilabel_precision(preds, labels, num_labels=NUM_LABELS)
-        # recall_macro = multilabel_recall(preds, labels, num_labels=NUM_LABELS)
-        f1_macro = multilabel_f1_score(preds, labels, num_labels=num_labels)
-
-        # AUROC score of 1 is a perfect score
-        # AUROC score of 0.5 corresponds to random guessing.
-        # auroc_macro = multilabel_auroc(
-        #     preds, labels, num_labels=num_labels, average="macro", thresholds=None
-        # )
-
-        metrics = {
-            "accuracy_exact": acc_exact,
-            "accuracy_micro": np.nan,
-            "accuracy_macro": acc_macro,
-            # 'precision_macro': precision_macro,
-            # 'recall_macro':    recall_macro,
-            "f1_macro": f1_macro,
-            # "AUROC_macro": auroc_macro,
-        }
-    else:
-
-        acc_micro = multiclass_accuracy(
-            preds, labels, num_classes=num_labels, average="micro"
-        )
-        acc_macro = multiclass_accuracy(
-            preds, labels, num_classes=num_labels, average="macro"
-        )
-        # precision_macro = multiclass_precision(preds, labels, num_classes=NUM_LABELS)
-        # recall_macro = multiclass_recall(preds, labels, num_classes=NUM_LABELS)
-        f1_macro = multiclass_f1_score(preds, labels, num_classes=num_labels)
-
-        # auroc_macro = multiclass_auroc(
-        #     preds, labels, num_classes=num_labels, average="macro", thresholds=None
-        # )
-
-        metrics = {
-            "accuracy_exact": np.nan,
-            "accuracy_micro": acc_micro,
-            "accuracy_macro": acc_macro,
-            # 'precision_macro': precision_macro,
-            # 'recall_macro':    recall_macro,
-            "f1_macro": f1_macro,
-            # "AUROC_macro": auroc_macro,
-        }
-
-    return metrics
 
 
 def largest_multilabel(targets):
@@ -115,6 +39,8 @@ scores = pd.DataFrame()
 for target_col, info in target_problems.items():
     target_ids, targets, labels = get_labels(book_col, target_col)
     problem_type = info["problem_type"]
+    multi_label = True if problem_type == 'multilabel' else False
+
     num_labels = info["num_labels"]
 
     mask = np.isin(target_ids, book_ids)
@@ -122,12 +48,20 @@ for target_col, info in target_problems.items():
 
     if problem_type == "multilabel":
         preds = largest_multilabel(targets)
-        metrics = compute_metrics(preds, targets, problem_type, num_labels)
+
+        preds = torch.Tensor(preds)
+        targets = torch.tensor(targets)
+
+        metrics = compute_metrics(preds, targets, multi_label, num_labels)
         scores[target_col] = pd.Series(metrics, name=target_col, dtype="float")
 
     elif problem_type == "multiclass":
         preds = largest_multiclass(targets)
-        metrics = compute_metrics(preds, targets, problem_type, num_labels)
+        
+        preds = torch.Tensor(preds)
+        targets = torch.tensor(targets)
+
+        metrics = compute_metrics(preds, targets, multi_label, num_labels)
         scores[target_col] = pd.Series(metrics, name=target_col, dtype="float")
 
 print(((scores * 100).round(1)).astype(str))
