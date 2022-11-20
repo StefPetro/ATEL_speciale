fec188620a068329c082ba8966e50e1b58aaa454
Enny <ennytao@gmail.com>
2022-11-20 12:01:35 +0100

    added new metrics

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,9 +1,17 @@
 from torchmetrics.functional.classification import (
     multiclass_accuracy,
     multiclass_f1_score,
+    multiclass_precision,
+    multiclass_recall,
+    multilabel_stat_scores,
     multilabel_accuracy,
     multilabel_exact_match,
     multilabel_f1_score,
+    multilabel_precision,
+    multilabel_recall,
+    multiclass_stat_scores,
+    multilabel_specificity,
+    multiclass_specificity
 )
 
 import numpy as np
@@ -15,12 +23,20 @@ def compute_metrics(preds, targets, multi_label, output_size):
         acc_exact = multilabel_exact_match(preds, targets, num_labels=output_size)
         acc_macro = multilabel_accuracy(preds, targets, num_labels=output_size)
         f1_macro = multilabel_f1_score(preds, targets, num_labels=output_size)
+        precision_macro = multilabel_precision(preds, targets, num_labels=output_size)
+        recall_macro = multilabel_recall(preds, targets, num_labels=output_size)
+        specificity_macro = multilabel_specificity(preds, targets, num_labels=output_size)
+
+        # print(specificity_macro, (tn/(fp+tn)))
 
         metrics = {
             "acc_exact": acc_exact,
             "acc_micro": np.nan,
             "acc_macro": acc_macro,
             "f1_macro": f1_macro,
+            "precision_macro": precision_macro,
+            "recall_macro": recall_macro,
+            "fpr": 1-specificity_macro # fpr = fp/(fp+tn) = 1-tn/(fp+tn) = 1-specificity
         }
 
     else:
@@ -31,12 +47,18 @@ def compute_metrics(preds, targets, multi_label, output_size):
             preds, targets, num_classes=output_size, average="macro"
         )
         f1_macro = multiclass_f1_score(preds, targets, num_classes=output_size)
+        precision_macro = multiclass_precision(preds, targets, num_classes=output_size)
+        recall_macro = multiclass_recall(preds, targets, num_classes=output_size)
+        specificity_macro = multiclass_specificity(preds, targets, num_classes=output_size)
 
         metrics = {
             "acc_exact": np.nan,
             "acc_micro": acc_micro,
             "acc_macro": acc_macro,
             "f1_macro": f1_macro,
+            "precision_macro": precision_macro,
+            "recall_macro": recall_macro,
+            "fpr": 1-specificity_macro
         }
 
     return metrics
--- folder_metrics.py
+++ folder_metrics.py
@@ -44,12 +44,15 @@ for t in target_cols:
         target = y[val]
 
         # # For LSTM
-        # preds_dir = glob.glob(
-        #     f"./lightning_logs/{t.replace(' ', '_')}/*/bs_64/CV_{k+1}/*.pt"
-        # )
+        preds_dir = glob.glob(
+            f"./lightning_logs/{t.replace(' ', '_')}/*/bs_64/CV_{k+1}/*.pt"
+        )
 
-        # For HuggingFace
-        preds_dir = glob.glob(f"./huggingface_logs/{t}/*-241022/CV_{k+1}/*.pt")
+        # For BERT
+        # preds_dir = glob.glob(f"./bert_logs/{t.replace(' ', '_')}/*/CV_{k+1}/*.pt")
+
+        # For BørneBÆRTa
+        # preds_dir = glob.glob(f"./warmup_logs/{t}/*/CV_{k+1}/*.pt")
 
         assert len(preds_dir) == 1, "There must only be 1 set of predictions"
 
@@ -57,17 +60,16 @@ for t in target_cols:
 
         if multi_label:
             logit_func = nn.Sigmoid()
-            preds = logit_func(preds)
+            preds = logit_func(torch.Tensor(preds))
         else:
             # Take the Sigmoid of preds
             logit_func = nn.Softmax()
-            preds = logit_func(preds)
+            preds = logit_func(torch.Tensor(preds))
 
         metrics.append(
             compute_metrics(preds, torch.Tensor(target), multi_label, num_labels)
         )
-
-        break
+        
 
     metrics_df = pd.DataFrame(metrics)
     k_scores = metrics_df.mean(axis=0)
