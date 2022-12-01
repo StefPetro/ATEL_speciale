071781805c870bbe9fc9e361f9f0cdee7100d502
Stefan Petrovic <s173991@student.dtu.dk>
2022-12-01 10:29:09 +0100

    Changed entropy function to use logits

--- acquisition_functions.py
+++ acquisition_functions.py
@@ -1,9 +1,14 @@
 import torch
 
-def calc_entropy(pk, problem_type='multi_class'):
-    idx = pk > 0
+def calc_entropy(logits, problem_type='multi_class'):
     if problem_type == 'multi_class':
-        H = -torch.sum(pk[idx] * torch.log2(pk[idx]))
+        logit_func = torch.nn.Sigmoid()
+        probs = logit_func(logits)
+        idx = probs > 0
+        H = -torch.sum(probs[idx] * torch.log2(probs[idx]))
     elif problem_type == 'multi_label':
+        logit_func = torch.nn.Softmax()
+        probs = logit_func(logits)
+        idx = probs > 0
         H = None
     return H
