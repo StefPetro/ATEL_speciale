3fa60ac39be373384a2744c8d650f58197949d26
ennytao <ennytao@gmail.com>
2023-01-13 14:46:03 +0100

    updated batch scripts for AL

---
+++ batch_scripts/AL_all.sh
@@ -0,0 +1,16 @@
+#!/bin/sh 
+
+list="2 3 4 5 6 7 8 9 10"
+target="Genre Tekstbaand Fremstillingsform Semantisk_univers Stemmer Perspektiv Holistisk_vurdering"
+acq_function="random"
+
+for a in $acq_function
+do
+    for l in $list
+    do
+        for t in $target
+        do
+            bsub < ./batch_scripts/AL_scripts/$a/$t/AL_${t}_cv$l.sh
+        done
+    done
+done
--- batch_scripts/RoBERTa_scripts/roberta_Perspektiv.sh
+++ batch_scripts/RoBERTa_scripts/roberta_Perspektiv.sh
@@ -17,7 +17,7 @@ module load cudnn/v8.3.0.98-prod-cuda-11.5
 ### -- set the job Name -- 
 #BSUB -J roberta_Perspektiv
 ### -- ask for number of cores (default: 1) -- 
-#BSUB -n 4
+#BSUB -n 2
 ### -- specify that the cores must be on the same host -- 
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 2GB of memory per core/slot -- 
